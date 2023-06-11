4c9925f44e1828695360faf75d8301245f858706
Katiuska Alvarez Agamez <katiuska-alvarez@hotmail.com>
2023-06-10 23:12:26 -0500

    Crear entidad Documento y propiedades de navegaci[on

---
+++ FEDRA/Fedra.Data/Entities/ConfiguracionDocumento.cs
@@ -0,0 +1,19 @@
+﻿namespace Fedra.Data.Entities
+{
+    internal class ConfiguracionDocumento
+    {
+        public long Id { get; set; }
+        public long EmpresaId { get; set; }
+        public string NombreDocumento { get; set; } = string.Empty;
+        public string Codigo { get; set; } = string.Empty;
+        public string Prefijo { get; set; } = string.Empty;
+        public string Sufijo { get; set; } = string.Empty;
+        public bool AfectaInventario { get; set; }
+        public long ConsecutivoInicial { get; set; }
+        public long ConsecutivoActual { get; set; }
+        public long ConsecutivoFinal { get; set; }
+        public string Mensaje { get; set; } = string.Empty;
+        public string Mensaje2 { get; set; } = string.Empty;
+        
+    }
+}
---
+++ FEDRA/Fedra.Data/Entities/Documento.cs
@@ -0,0 +1,38 @@
+﻿namespace Fedra.Data.Entities
+{
+    internal class Documento
+    {
+        public long Id { get; set; }
+        public long TipoConfiguracionDocumentoId { get; set; }
+        public string CodigoBarras { get; set; } = string.Empty;
+        public long Consecutivo { get; set; }
+        public long TerceroId { get; set; }
+        public DateTime Fecha { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public Decimal Subtotal { get; set; }
+        public Decimal Ajuste { get; set; }
+        public Decimal Descuento { get; set; }
+        public Decimal Total { get; set; }
+        public int Estado { get; set; }
+        public long EmpresaId { get; set; }
+        public int Condicion { get; set; }
+        public long FormaPagoId { get; set; }
+        public string Referencia { get; set; } = string.Empty;
+        public Decimal DineroRecibido { get; set; }
+        public Decimal Cambio { get; set; }
+        public string CreadoPor { get; set; } = string.Empty;
+        public DateTime FechaCreacion { get; set; }
+        public string ModificadoPor { get; set; } = string.Empty;
+        public DateTime FechaModificacion { get; set; }
+
+        /// <summary>
+        /// Propiedades de navegacion
+        /// </summary>
+
+        public ConfiguracionDocumento? ConfiguracionDocumento { get; set; }
+        public Tercero? Tercero { get; set; }
+        public FormaPago? FormaPago { get; set; }
+
+
+    }
+}

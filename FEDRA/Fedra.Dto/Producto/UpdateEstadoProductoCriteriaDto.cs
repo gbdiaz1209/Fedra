52e363a79d08edc422127b70629ee03ae9a2e89e
GABRIEL DIAZ <GABRIEL DIAZ@DESKTOP-0LC1T7B>
2023-06-17 12:11:50 -0500

    actualizacion de GetbyEstado en Api

--- FEDRA/Fedra.Dto/Producto/UpdateEstadoProductoCriteriaDto.cs
+++ FEDRA/Fedra.Dto/Producto/UpdateEstadoProductoCriteriaDto.cs
@@ -2,14 +2,12 @@
 {
     public class UpdateEstadoProductoCriteriaDto
     {
-        public object ProductoId;
-
         public long Id { get; set; }
 
         public int Estado { get; set; }
 
         public string? ModificadoPor { get; set; }
-        public string Nombre { get; set; }
+   
     }
 }
 

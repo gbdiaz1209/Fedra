88fd5159c91381a0c18ca10c5e06e2bd004629f8
Óscar José Sánchez Calle <oscar.jsc@outlook.com>
2023-07-02 17:15:46 -0500

    Menu navegacion documentos, iconos

--- ReactUI/fedra-ui/src/components/layout/TopNav/TopNav.js
+++ ReactUI/fedra-ui/src/components/layout/TopNav/TopNav.js
@@ -3,9 +3,7 @@ import './TopNav.css'
 
 import { BsPeople, BsBox, BsHouse } from 'react-icons/bs';
 import { TbFileInvoice } from 'react-icons/tb';
-import { SlNotebook } from 'react-icons/sl';
 import { CgMenuGridO } from 'react-icons/cg';
-import { FaAngleDown } from 'react-icons/fa';
 import { BiMoon, BiSun } from 'react-icons/bi';
 import { BsSun } from 'react-icons/bs';
 
@@ -39,6 +37,155 @@ export const TopNav = () => {
     </div>
     <div className="header-nav ">
       <div className="nav-item">
+        <a href="https://www.mockplus.com/enterprise" className="first-nav">
+          <BsHouse size={18}/>&nbsp;&nbsp;Dashboard          
+        </a>
+      </div>      
+      <div className="nav-item">
+        <a href="https://www.mockplus.com/enterprise" className="first-nav">
+          <TbFileInvoice size={18} />&nbsp;&nbsp;Venta Rápida
+        </a>
+      </div>
+      <div className="nav-item">
+         <span className="first-nav">
+            <CgMenuGridO size={18} />&nbsp;&nbsp;Documentos        
+            <i className="iconfont icon_broad_back" />
+         </span>
+        <div className="second-menu">
+          <div className="left-list">
+            <div className="top-titles">Principales</div>
+            <a
+              href="#"
+              style={{ backgroundImage: 'url("../images/nav/pic1.png")' }}
+            >
+              
+              <span>Facturas</span>
+              <p>Gestiona las ventas</p>
+            </a>
+            <a
+              href="#"
+              style={{ backgroundImage: 'url("../images/nav/compras.png")' }}
+            >
+              <span>Compras</span>
+              <p>Gestion de compras a proveedores</p>
+            </a>
+            <a
+              href="#"
+              style={{ backgroundImage: 'url("../images/nav/edit.png")' }}
+            >
+              <span>Comprobante de Egreso</span>
+              <p>Controla los gastos del negocio</p>
+            </a>
+            <a
+              style={{ backgroundImage: 'url("../images/nav/recibo-caja.png")' }}
+              id="head-nav-download-rp"
+            >
+              <span>Recibo d Caja</span>
+              <p>Historial de recibos</p>
+            </a>
+            <div className="top-titles-line">
+              Necesitas una cotización de venta？
+            </div>
+            <a
+              href="#"
+              style={{ backgroundImage: 'url("../images/nav/help-call.png")' }}
+            >
+              <span>Cotizaciones</span>
+              <p>Genera cotizaciones rapidamente</p>
+            </a>
+          </div>
+          <div className="line" />
+          <div className="right-cons">
+            <div className="top-titles">
+              Otros              
+            </div>
+            <a href="#" className="a1">
+              Comprobantre de Ingreso
+            </a>
+            <a href="#" className="a1">
+              Devolucion en venta
+            </a>
+            <a href="#" className="a1">
+              Devolucion en Compra
+            </a>           
+            <div className="top-titles">Ajuste de inventario</div>
+            <a href="#" className="a1">
+              Entrada de Inventario
+            </a>
+            <a
+              href="#"
+              className="a1"
+            >
+              Salida de Inventario
+            </a>
+            <a href="@" className="a1">
+              Inventario Inicial
+            </a>           
+            <div className="top-titles">Ajustes</div>
+            <a
+              href="#"
+              className="a1"
+            >
+              Conficuración de documentos{" "}
+            </a>
+          </div>
+        </div>
+      </div>    
+      <div className="nav-item">
+        <a href="https://www.mockplus.com/enterprise" className="first-nav">
+        <BsPeople size={18} />&nbsp;&nbsp;Terceros
+        </a>
+      </div>  
+      <div className="nav-item">
+        <span className="first-nav">
+            <BsBox size={18} />&nbsp;&nbsp;Productos
+          <i className="iconfont icon_broad_back" />
+        </span>
+        <div className="second-menu" style={{ width: 252, padding: 16 }}>
+          <a href="#" className="price-a">
+            <img src="../images/nav/rp1.png" alt="" />
+            Inventario
+          </a>
+          <a href="mockplus-cloud.html" className="price-a">
+            <img src="../images/nav/cc.png" alt="" />
+            Categorías
+          </a>
+        </div>
+      </div>
+      <div className="nav-item">
+        <span className="first-nav">
+           <BsPeople size={18} />&nbsp;&nbsp;Cuentas
+          <i className="iconfont icon_broad_back" />
+        </span>
+        <div className="second-menu" style={{ width: 252, padding: 16 }}>
+          <a href="#" className="price-a">
+            <img src="../images/nav/rp1.png" alt="" />
+            Cuentas por Cobrar
+          </a>
+          <a href="mockplus-cloud.html" className="price-a">
+            <img src="../images/nav/cc.png" alt="" />
+            Cuentas por Pagar
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className="right-login">             
+      <div onClick={handleChange} style={{paddingTop: "12px"}}>    
+        {currentTheme == "dark" ? <BsSun size={19}/> : <BiMoon size={19} />}
+      </div>
+    </div>
+    <div className="iconfont icon_menu icon-color" />
+    <div className="iconfont icon_menu_close icon-color" />
+  </div>
+</header>
+
+    </>
+  )
+}
+
+
+
+{/* <div className="nav-item">
         <span className="first-nav">
         <BsHouse size={18}/>&nbsp;&nbsp;Dashboard
           <i className="iconfont icon_broad_back" />
@@ -130,146 +277,91 @@ export const TopNav = () => {
             </a>
           </div>
         </div>
-      </div>
-      <div className="nav-item">
-        <a href="https://www.mockplus.com/enterprise" className="first-nav">
-          <TbFileInvoice size={18} />&nbsp;&nbsp;Venta Rápida
-        </a>
-      </div>
-      <div className="nav-item">
-        <span className="first-nav">
-        <CgMenuGridO size={18} />&nbsp;&nbsp;Documentos        
-          <i className="iconfont icon_broad_back" />
-        </span>
-        <div className="second-menu">
-          <div className="left-list">
-            <div className="top-titles">Solutions</div>
-            <a
-              href="https://www.mockplus.com/cloud?home=1&type=designer#collaboration"
-              style={{ backgroundImage: 'url("../images/nav/pic5.png")' }}
-            >
-              <span>For Designers</span>
-              <p>Import Figma, Sketch, XD and PS files</p>
-            </a>
-            <a
-              href="https://www.mockplus.com/cloud?home=1&type=developer#collaboration"
-              style={{ backgroundImage: 'url("../images/nav/pic6.png")' }}
-            >
-              <span>For Developers</span>
-              <p>Get all the assets, specs and code</p>
-            </a>
-            <a
-              href="https://www.mockplus.com/cloud?home=1&type=team#collaboration"
-              style={{ backgroundImage: 'url("../images/nav/pic7.png")' }}
-            >
-              <span>For Product Teams</span>
-              <p>Manage projects and product updates</p>
-            </a>
-            <div className="top-titles">Learn</div>
-            <a
-              href="https://www.mockplus.com/learn/what-is-mockplus-cloud"
-              className="a1"
-            >
-              What is Mockplus Cloud ?{" "}
-              <i className="iconfont icon_tag_right_arrow" />
-            </a>
-          </div>
-          <div className="line" />
-          <div className="right-cons">
-            <div className="top-titles">
-              Integrations
-              <a
-                href="https://www.mockplus.com/cloud-integrations"
-                className="more"
-              >
-                See all <i className="iconfont icon_tag_right_arrow" />
-              </a>
-            </div>
-            <a href="https://help.mockplus.com/p/498" className="a1">
-              Figma
-            </a>
-            <a href="https://help.mockplus.com/p/249" className="a1">
-              Photoshop
-            </a>
-            <a href="https://help.mockplus.com/p/251" className="a1">
-              Adobe XD
-            </a>
-            <a href="https://help.mockplus.com/p/250" className="a1">
-              Sketch
-            </a>
-            <div className="top-titles">Why Mockplus Cloud</div>
-            <a
-              href="https://www.mockplus.com/mockplus-vs-invision"
-              className="a1"
-            >
-              Compare InVision
-            </a>
-            <a
-              href="https://www.mockplus.com/mockplus-vs-zeplin"
-              className="a1"
-            >
-              Compare Zeplin
-            </a>
-            <a
-              href="https://www.mockplus.com/remote-design-collaboration"
-              className="a1"
-            >
-              Collaborate remotely
-            </a>
-            <a href="https://www.mockplus.com/mockplus-ds" className="a1">
-              Manage design systems
-            </a>
-          </div>
-        </div>
-      </div>     
-      <div className="nav-item">
-        <a href="https://www.mockplus.com/enterprise" className="first-nav">
-        <BsPeople size={18} />&nbsp;&nbsp;Terceros
-        </a>
-      </div>  
-      <div className="nav-item">
-        <span className="first-nav">
-            <BsBox size={18} />&nbsp;&nbsp;Productos
-          <i className="iconfont icon_broad_back" />
-        </span>
-        <div className="second-menu" style={{ width: 252, padding: 16 }}>
-          <a href="mockplus-rp.html" className="price-a">
-            <img src="../images/nav/rp1.png" alt="" />
-            Inventario
-          </a>
-          <a href="mockplus-cloud.html" className="price-a">
-            <img src="../images/nav/cc.png" alt="" />
-            Categorías
-          </a>
-        </div>
-      </div>
-      <div className="nav-item">
-        <span className="first-nav">
-           <BsPeople size={18} />&nbsp;&nbsp;Cuentas
-          <i className="iconfont icon_broad_back" />
-        </span>
-        <div className="second-menu" style={{ width: 252, padding: 16 }}>
-          <a href="mockplus-rp.html" className="price-a">
-            <img src="../images/nav/rp1.png" alt="" />
-            Cuentas por cobrar
-          </a>
-          <a href="mockplus-cloud.html" className="price-a">
-            <img src="../images/nav/cc.png" alt="" />
-            Cuentas por Pagar
-          </a>
-        </div>
-      </div>
-    </div>
-    <div className="right-login">             
-      <div onClick={handleChange} style={{paddingTop: "12px"}}>    
-        {currentTheme == "dark" ? <BsSun size={19}/> : <BiMoon size={19} />}
-      </div>
-    </div>
-    <div className="iconfont icon_menu icon-color" />
-    <div className="iconfont icon_menu_close icon-color" />
-  </div>
-</header>
+      </div> */}
 
-    </>
-  )
-}
+    //   <div className="nav-item">
+    //   <span className="first-nav">
+    //   <CgMenuGridO size={18} />&nbsp;&nbsp;Documentos        
+    //     <i className="iconfont icon_broad_back" />
+    //   </span>
+    //   <div className="second-menu">
+    //     <div className="left-list">
+    //       <div className="top-titles">Solutions</div>
+    //       <a
+    //         href="https://www.mockplus.com/cloud?home=1&type=designer#collaboration"
+    //         style={{ backgroundImage: 'url("../images/nav/pic5.png")' }}
+    //       >
+    //         <span>For Designers</span>
+    //         <p>Import Figma, Sketch, XD and PS files</p>
+    //       </a>
+    //       <a
+    //         href="https://www.mockplus.com/cloud?home=1&type=developer#collaboration"
+    //         style={{ backgroundImage: 'url("../images/nav/pic6.png")' }}
+    //       >
+    //         <span>For Developers</span>
+    //         <p>Get all the assets, specs and code</p>
+    //       </a>
+    //       <a
+    //         href="https://www.mockplus.com/cloud?home=1&type=team#collaboration"
+    //         style={{ backgroundImage: 'url("../images/nav/pic7.png")' }}
+    //       >
+    //         <span>For Product Teams</span>
+    //         <p>Manage projects and product updates</p>
+    //       </a>
+    //       <div className="top-titles">Learn</div>
+    //       <a
+    //         href="https://www.mockplus.com/learn/what-is-mockplus-cloud"
+    //         className="a1"
+    //       >
+    //         What is Mockplus Cloud ?{" "}
+    //         <i className="iconfont icon_tag_right_arrow" />
+    //       </a>
+    //     </div>
+    //     <div className="line" />
+    //     <div className="right-cons">
+    //       <div className="top-titles">
+    //         Integrations
+    //         <a
+    //           href="https://www.mockplus.com/cloud-integrations"
+    //           className="more"
+    //         >
+    //           See all <i className="iconfont icon_tag_right_arrow" />
+    //         </a>
+    //       </div>
+    //       <a href="https://help.mockplus.com/p/498" className="a1">
+    //         Figma
+    //       </a>
+    //       <a href="https://help.mockplus.com/p/249" className="a1">
+    //         Photoshop
+    //       </a>
+    //       <a href="https://help.mockplus.com/p/251" className="a1">
+    //         Adobe XD
+    //       </a>
+    //       <a href="https://help.mockplus.com/p/250" className="a1">
+    //         Sketch
+    //       </a>
+    //       <div className="top-titles">Why Mockplus Cloud</div>
+    //       <a
+    //         href="https://www.mockplus.com/mockplus-vs-invision"
+    //         className="a1"
+    //       >
+    //         Compare InVision
+    //       </a>
+    //       <a
+    //         href="https://www.mockplus.com/mockplus-vs-zeplin"
+    //         className="a1"
+    //       >
+    //         Compare Zeplin
+    //       </a>
+    //       <a
+    //         href="https://www.mockplus.com/remote-design-collaboration"
+    //         className="a1"
+    //       >
+    //         Collaborate remotely
+    //       </a>
+    //       <a href="https://www.mockplus.com/mockplus-ds" className="a1">
+    //         Manage design systems
+    //       </a>
+    //     </div>
+    //   </div>
+    // </div> 
